a3f2e4202b6e0a596dc1a6d6f3c131e20de134f5
dawneraq <dawneraq@gmail.com>
2020-08-12 17:27:39 -0700

    add API key to environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-weather';
+
+  ngOnInit() {
+    console.log('weather API key:', environment.WEATHER_API_KEY);
+  }
 }
