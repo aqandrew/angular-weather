6667f5da3fb6a87125776a36bddf6e410c98425f
dawneraq <dawneraq@gmail.com>
2020-08-12 19:20:31 -0700

    reuse reference to component between tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,32 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [ReactiveFormsModule],
-    }).compileComponents();
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.componentInstance;
+      });
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-weather'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-weather');
   });
 
   it('should say hello with a sun emoji', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('main h1').textContent).toContain(
@@ -32,21 +35,15 @@ describe('AppComponent', () => {
   });
 
   it('should be invalid when city is empty', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.weatherForm.valid).toBeFalse();
   });
 
   it('should valid when city is not empty', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     app.weatherForm.controls['city'].setValue('foo');
     expect(app.weatherForm.valid).toBeTrue();
   });
 
   it('should set submitted to true', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     app.onSubmit();
     expect(app.submitted).toBeTrue();
   });
